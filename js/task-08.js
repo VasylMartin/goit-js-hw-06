1997a4f92842ad0c5259c061ae92607ff3ebee95
VasylMartin <vasiliymartiniouk@gmail.com>
2022-07-10 17:11:49 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", login);
 
+function login(event) {
+  event.preventDefault();
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+
+  const formData = {
+    email: formEl.email.value,
+    password: formEl.password.value,
+  };
+  console.log(formData);
+  form.reset();
+
+  if (email === "" || password === "") {
+    alert("Empty form");
+    form.reset();
+    return;
+  }
+}
