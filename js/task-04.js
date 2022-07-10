afe09732838e6c4423cacc2b96eb4499a0375d60
VasylMartin <vasiliymartiniouk@gmail.com>
2022-07-10 14:27:53 +0300

    task 5 ???

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const counter = document.querySelector("#value");
+const counterValue = 0;
+// const btns = document.querySelectorAll("button");
+const btnD = document.querySelector('[data-action="decrement"]');
+const btnI = document.querySelector('[data-action="increment"]');
+// console.log(btns);
+
+const clickDecrement = () => {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+};
+
+const clickIncrement = () => {
+  counterValue += 1;
+  counter.textContent = counterValue;
+};
+
+btnD.addEventListener("click", clickDecrement);
+btnI.addEventListener("click", clickIncrement);
