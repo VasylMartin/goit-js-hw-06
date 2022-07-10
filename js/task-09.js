9c212e8d1b93ce0675fc03fbd1a5fecc5936c039
VasylMartin <vasiliymartiniouk@gmail.com>
2022-07-10 17:52:58 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+let text = document.querySelector(".color");
+// console.log(body);
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  text.textContent = getRandomHexColor();
+}
+
+btn.addEventListener("click", changeColor);
